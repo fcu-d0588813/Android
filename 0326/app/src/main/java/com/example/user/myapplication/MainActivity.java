c76609470905bf330fa175629a968436fd13666e
FC01\user <zengla0921@gmail.com>
2018-03-26 16:04:21 +0800

    0326

---
+++ 0326/app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.user.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RadioGroup rg = findViewById(R.id.drink);
+        rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if(checkedId==R.id.bu1){
+                    Toast.makeText(MainActivity.this,Integer.toString(checkedId),Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+    }
+
+}
